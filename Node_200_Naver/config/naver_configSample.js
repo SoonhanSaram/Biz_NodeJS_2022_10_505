b27e23cfebc898b949d5907a9f97aa7e1b87d075
SoonhanSaram <kyengmin911@naver.com>
2023-01-13 09:27:39 +0900

    01132

---
+++ Node_200_Naver/config/naver_configSample.js
@@ -0,0 +1,10 @@
+/**
+ * API 사용을 위한 기본 설정 페이지
+ */
+
+const CLIENT_ID = { KEY: "X-Naver-Client-Id", VALUE: "" };
+const CLIENT_SECRET = { KEY: "X-Naver-Client-Secret", VALUE: "" };
+const BOOK_URL_JSON = "";
+const papago_URL_JSON = "";
+
+export default (CLIENT_ID, CLIENT_SECRET, BOOK_URL_JSON);
